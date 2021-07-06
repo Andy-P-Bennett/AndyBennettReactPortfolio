a73dc5cacaa7ca1aa6c98ddc16a43de2ff246824
LittlestBits <65984770+LittlestBits@users.noreply.github.com>
2021-07-06 11:55:19 -0600

    07/6/21

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from "axios";
 
 export default class Login extends Component {
     constructor(props) {
@@ -7,7 +7,8 @@ export default class Login extends Component {
 
         this.state = {
             email: "",
-            password: ""
+            password: "",
+             errorText: ""
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -16,7 +17,8 @@ export default class Login extends Component {
 
     handleChange(event) {
         this.setState({
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
+            errorText: ""
         })
     }
 
@@ -28,10 +30,25 @@ export default class Login extends Component {
                 password: this.state.password
             }
         },
-        { withCredentials: true}
-        ).then(response => {
-            console.log("response", response)
+        { withCredentials: true }
+        )
+        .then(response => {
+            if (response.data.status === 'created') {
+                console.log("open sesame")
+               this.props.handleSuccessfulAuth();
+            } else {
+                this.setState ({
+                    errorText: "Wrong email or password"
+                });
+                this.props.handleUnsuccessfulAuth();
+            }
         })
+        .catch(error => {
+            this.setState({
+                errorText: "An error occured"
+            });
+            this.props.handleUnsuccessfulAuth();
+        });
         
         event.preventDefault();
         
@@ -41,6 +58,9 @@ export default class Login extends Component {
         return (
             <div>
                 <h1>Login to Access your Dashboard</h1>
+
+                <div>{this.state.errorText}</div>
+
                 <form onSubmit={this.handleSubmit}>
                     <input 
                         type="email"
